a8aceb6e1101cf230c8eb5917cbed1cdffd8cbaf
Elias Neto <netoeac.dev@gmail.com>
2024-06-10 02:40:11 -0300

    fix

--- components/greenButton.tsx
+++ components/greenButton.tsx
@@ -19,13 +19,13 @@ const GreenButton = (props: GreenButtonProps) => {
     <div className={encode.className}>
       <button className=" flex  gap-4 bg-[#a7b928] py-3 px-6 uppercase font-bold rounded-md font-econdensed hover:bg-[#303436] hover:text-white transition-all duration-500">
         {props.text}
-        <Image
+        {/* <Image
           src={props.icon}
           alt="icon "
           height={24}
           width={24}
           className="hover:text-white"
-        />
+        /> */}
       </button>
     </div>
   );
